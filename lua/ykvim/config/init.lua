2917810a7cc0a1f669de14c83612e4e52ff8acc7
kuechlin <y.kuechlin@vws.app>
2023-11-10 12:56:31 +0100

    cleanup init.lua
    moved config to config folder

--- lua/ykvim/config/init.lua
+++ lua/ykvim/config/init.lua
@@ -1,11 +1,14 @@
 local M = {}
 
-function _load(name)
-  require("ykvim.config." .. name)
-end
-
 function M.setup()
+  function _load(name)
+    require("ykvim.config." .. name)
+  end
+
+  _load("settings")
   _load("keymaps")
+  _load("lsp")
+  _load("cmp")
 end
 
 return M
